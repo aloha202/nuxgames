fc67a3ddbbfe247db9bc4aecd8493e97776b6e00
Alexey Radyuk <alexey.radyuk@gmail.com>
2025-02-21 21:40:52 +0200

    startup fix

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cp .env.example .env
 composer install --no-interaction --prefer-dist
 npm install && npm run build
 php artisan key:generate
 php artisan migrate --force
 bash scheduler.sh &
-php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8001
